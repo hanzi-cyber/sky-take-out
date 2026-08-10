dbe22b78ae784f3346c21c662bf7cc273f4357ae
hanzi <hanzi819@qq.com>
2026-08-10 15:37:18 +0800

    新增菜品业务代码

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,26 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.DishDTO;
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/dish")
+public class DishController {
+    /**
+     * 新增菜品
+     */
+    @Autowired
+    private DishService dishService;
+    @PostMapping
+    public Result save(@RequestBody DishDTO dishDTO){
+
+        dishService.save(dishDTO);
+        return Result.success();
+    }
+}
