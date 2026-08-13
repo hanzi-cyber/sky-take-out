0b96c485c1a07a15c15ba910f38244845975c75c
hanzi <hanzi819@qq.com>
2026-08-13 21:18:39 +0800

    购物车查看业务代码

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -1,13 +1,15 @@
 package com.sky.controller.user;
 
+import com.sky.context.BaseContext;
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.ShoppingCartMapper;
 import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("user/shoppingCart")
@@ -16,9 +18,20 @@ public class ShoppingCartController {
     @Autowired
     private ShoppingCartService shoppingCartService;
 
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+
     @PostMapping("/add")
     public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
         shoppingCartService.add(shoppingCartDTO);
         return Result.success();
+    }
+
+    @GetMapping("/list")
+    public Result<List<ShoppingCart>> list() {
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder().userId(userId).build();
+        List<ShoppingCart> shoppingCartList = shoppingCartMapper.get(shoppingCart);
+        return Result.success(shoppingCartList);
     }
 }
