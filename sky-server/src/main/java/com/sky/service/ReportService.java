56191515a667c2cdcfd340be3af99b9d44124d21
hanzi <hanzi819@qq.com>
2026-08-15 18:23:08 +0800

    用户统计和营业额统计业务代码

---
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -0,0 +1,12 @@
+package com.sky.service;
+
+import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
+
+import java.time.LocalDate;
+
+public interface ReportService {
+    TurnoverReportVO getTurnoverReport(LocalDate begin, LocalDate end);
+
+    UserReportVO getUserReport(LocalDate begin, LocalDate end);
+}
