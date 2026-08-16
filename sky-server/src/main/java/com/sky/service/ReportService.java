986d0487e2a688b3d4312d6a4085818a20305f53
hanzi <hanzi819@qq.com>
2026-08-16 11:11:20 +0800

    导出最近30天运营报表excel

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -5,6 +5,7 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 public interface ReportService {
@@ -15,4 +16,9 @@ public interface ReportService {
     OrderReportVO getOrderReport(LocalDate begin, LocalDate end);
 
     SalesTop10ReportVO getSalesTop10Report(LocalDate begin, LocalDate end);
+
+    /**
+     * 导出最近30天运营数据Excel报表
+     */
+    void exportBusinessData(HttpServletResponse response);
 }
